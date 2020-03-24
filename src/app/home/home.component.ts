34fc6f93de5b05c40af31ac503c04ff95d5eb19c
santoravi7 <santoravi7@gmail.com>
2020-03-25 03:48:50 +0530

    7 files modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from '../user'
+import { USER } from '../user-credentials';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  user:User[];
   constructor() { }
 
   ngOnInit() {
+    getUser();
+  }
+  getUser():void{
+    this.notebookService.getNotebooks()
+        .subscribe(notes => this.notebooks = notes);
   }
 
 }
